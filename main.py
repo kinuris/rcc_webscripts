65560968e9dd8ba4da72d789e60410a2fd7fdd56
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2025-07-25 11:22:20 +0800

    Added script analysis method

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/helloworld')
 def index():
     return "Hello, World!"
+
+@app.route('/receipt/analyze')
+def analyze_receipt():
+    return "Receipt analysis not implemented yet."
